2c7735fc90644a406f988b96bf161cf64e6aa4f4
Really-Dream <Really_Dream@126.com>
2018-05-21 21:57:53 +0800

    初始化与清理
    Signed-off-by: Really-Dream <Really_Dream@126.com>

---
+++ src/initAndCleanup/ExplicitStatic.java
@@ -0,0 +1,33 @@
+package initAndCleanup;
+
+/**
+ * Created by H.J
+ * 2018/5/21
+ */
+class Cup{
+    Cup(int market){
+        System.out.println("Cup(" + market +")");
+    }
+    void f(int market){
+        System.out.println("f(" + market + ")");
+    }
+}
+
+class Cups{
+    static Cup cup1;
+    static Cup cup2;
+    static {
+        cup1 = new Cup(1);
+        cup2 = new Cup(2);
+    }
+    Cups(){
+        System.out.println("Cups()");
+    }
+}
+
+public class ExplicitStatic {
+    public static void main(String args[]){
+        System.out.println("Inside main()");
+        Cups.cup1.f(99);
+    }
+}
